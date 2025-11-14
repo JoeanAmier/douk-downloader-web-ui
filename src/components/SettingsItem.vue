d7de1391ccb60d7d768f9ceb6911bb947dc7ca93
Quan <yonglelolu@foxmail.com>
2025-11-14 21:22:27 +0800

    feat(settings): integrate horizontal navigation menu

--- src/components/SettingsItem.vue
+++ src/components/SettingsItem.vue
@@ -2,16 +2,30 @@
 import BoolItem from '@/components/settings/BoolItem.vue'
 import NumberItem from '@/components/settings/NumberItem.vue'
 import ButtonItem from '@/components/settings/ButtonItem.vue'
-import { ElRow, ElCol, } from 'element-plus'
-import {ref} from 'vue'
+import { ElRow, ElCol, ElMenu, ElMenuItem } from 'element-plus'
+import { ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 
 const { t } = useI18n()
 const boolValue = ref(true)
 const numberValue = ref(1)
+const activeIndex = ref('1')
+const handleSelect = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
 </script>
 
 <template>
+  <el-menu
+    :default-active="activeIndex"
+    class="el-menu-demo"
+    mode="horizontal"
+    @select="handleSelect"
+  >
+    <el-menu-item index="1">{{ t('全局配置') }}</el-menu-item>
+    <el-menu-item index="2">{{ t('抖音配置') }}</el-menu-item>
+    <el-menu-item index="3">{{ t('TikTok 配置') }}</el-menu-item>
+  </el-menu>
   <BoolItem :text="t('是否下载作品音乐')" params="music" v-model:value="boolValue" />
   <NumberItem
     :text="t('文件下载最大线程数')"
